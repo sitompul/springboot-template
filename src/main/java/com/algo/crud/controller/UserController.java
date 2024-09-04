1a6b09fb19349d2e95f70c39859863974a2a1897
efs <elfinositompul@gmail.com>
2024-09-04 11:08:44 +0700

    Finish java springboot template

---
+++ src/main/java/com/algo/crud/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.algo.crud.controller;
+
+import com.algo.crud.model.User;
+import com.algo.crud.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+      return userRepository.findAll();
+    }
+}
--- src/main/java/com/algo/crud/repository/UserRepository.java
+++ src/main/java/com/algo/crud/repository/UserRepository.java
@@ -1,2 +1,7 @@
 package com.algo.crud.repository;
 
+import com.algo.crud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
